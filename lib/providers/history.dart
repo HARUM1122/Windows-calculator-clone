b1027d7465a2ad56ebdeea6a0f4c8d5c14e6fa5b
HELINITY <>
2023-12-06 10:18:25 +0500

    .

--- lib/providers/history.dart
+++ lib/providers/history.dart
@@ -1,16 +1,29 @@
 import 'package:flutter/foundation.dart';
-
+import 'package:shared_preferences/shared_preferences.dart';
 class HistoryProvider extends ChangeNotifier{
-  final List<String> standardHistory = []; // expression:answer
-  final List<String> scientificHistory = []; // expression:answer
+  List<String> standardHistory = []; // expression:answer
+  List<String> scientificHistory = []; // expression:answer
+  final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+  HistoryProvider(){
+    _prefs.then((SharedPreferences prefs){
+      standardHistory = prefs.getStringList('standardCalcHistory')??[];
+      scientificHistory = prefs.getStringList('scientificCalcHistory')??[];
+    });
+  }
   void addToHistory(String ans,{bool standard=true}){
     if(standard){
+      if(standardHistory.length==60){
+        standardHistory.removeLast();
+      }
       standardHistory.insert(0,ans);
     }
     else{
+      if(scientificHistory.length==60){
+        scientificHistory.removeLast();
+      }
       scientificHistory.insert(0,ans);
     }
-    notifyListeners();
+    saveHistory(standard:standard);
   }
   void removeFromHistory(int index,{bool standard=true}){
     if(standard){
@@ -19,7 +32,7 @@ class HistoryProvider extends ChangeNotifier{
     else{
       scientificHistory.removeAt(index);
     }
-    notifyListeners();
+    saveHistory(standard:standard);
   }
   void clearHistory({bool standard=true}){
     if(standard && standardHistory.isNotEmpty){
@@ -27,6 +40,16 @@ class HistoryProvider extends ChangeNotifier{
     }
     else if(!standard&&scientificHistory.isNotEmpty){
       scientificHistory.clear();
+    }
+    saveHistory(standard:standard);
+  }
+  void saveHistory({bool standard=true})async{
+    final SharedPreferences prefs = await _prefs;
+    if(standard){
+      prefs.setStringList('standardCalcHistory', standardHistory);
+    }
+    else{
+      prefs.setStringList('scientificCalcHistory', scientificHistory);
     }
     notifyListeners();
   }
